2f2f73c99165c775d96555935da40ee401b8fbaf
gacfrs <gacfrs@gmail.com>
2017-03-29 14:10:03 -0300

    Add files via upload

---
+++ Events.c
@@ -0,0 +1,140 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : Projeto1-Semaforo-dimiefrauches
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2017-03-13, 14:23, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+        
+
+/*	##########################################################################
+ * 	EA076 C - Projeto 1 - Events.c
+ * 	Dimitri Reis			RA 145869
+ * 	Guilherme Frauches	RA 155591
+ *
+ *  O programa simula um semáforo inteligente com sensor de luminosidade
+ *  e detecção de pedestres.
+ * 
+ *	##########################################################################*/
+
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/* Link das variáveis de controle do tempo*/
+extern int cont;
+extern int cont_max;
+
+/* Link das variáveis de controle dos periféricos */
+extern int button;
+extern int noite;
+uint8_t conversor;				// Variável de 8 bits que irá operar como ADC
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void)
+{
+}
+
+/*
+** ===================================================================
+**     Event       :  TI1_OnInterrupt (module Events)
+**
+**     Component   :  TI1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void TI1_OnInterrupt(void){ 
+	AD1_Measure(FALSE);			// Faz a leitura do LDR e pula para rotina AD1_OnEnd
+	button = BUTTON_GetVal();	// Verifica o estaedo do botão (1 - pressionado, 0 - c.c.)
+	cont++;						// Faz a contagem do tempo
+
+	if (cont >= cont_max)		// Reinicia o ciclo do contador
+		cont = 0;
+}
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AD1_OnEnd(void){
+	AD1_GetValue8(&conversor);	// Converte o valor do LDR em 8 bits
+	if(conversor>128)			// Define o estado da variável "noite"
+		noite=1;				// 1 - Escuro
+	else
+		noite=0;				// 0 - Claro
+}
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnCalibrationEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called when the calibration has been finished.
+**         User should check if the calibration pass or fail by
+**         Calibration status method./nThis event is enabled only if
+**         the <Interrupt service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AD1_OnCalibrationEnd(void)
+{
+}
+	/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ main.c
@@ -0,0 +1,145 @@
+/* ###################################################################
+ **     Filename    : main.c
+ **     Project     : Projeto1-Semaforo-dimiefrauches
+ **     Processor   : MKL25Z128VLK4
+ **     Version     : Driver 01.01
+ **     Compiler    : GNU C Compiler
+ **     Date/Time   : 2017-03-13, 14:23, # CodeGen: 0
+ **     Abstract    :
+ **         Main module.
+ **         This module contains user's application code.
+ **     Settings    :
+ **     Contents    :
+ **         No public methods
+ **
+ ** ###################################################################*/
+/*!
+ ** @file main.c
+ ** @version 01.01
+ ** @brief
+ **         Main module.
+ **         This module contains user's application code.
+ */         
+/*!
+ **  @addtogroup main_module main module documentation
+ **  @{
+ */         
+/* MODULE main */
+
+/*	##########################################################################
+ * 	EA076 C - Projeto 1 - main.c
+ * 	Dimitri Reis			RA 145869
+ * 	Guilherme Frauches	RA 155591
+ *
+ *   O programa simula um semáforo inteligente com leds, sensor de luminosidade
+ *  detecção de pedestres.
+ *  
+ *   
+ * 
+ *	##########################################################################*/
+
+/* Inlcusão de módulos necessários para compilar o projeto */
+#include "Cpu.h"
+#include "Events.h"
+#include "LED_red_car.h"
+#include "BitIoLdd1.h"
+#include "TI1.h"
+#include "TimerIntLdd1.h"
+#include "TU1.h"
+#include "LED_yellow_car.h"
+#include "BitIoLdd4.h"
+#include "LED_green_car.h"
+#include "BitIoLdd5.h"
+#include "LED_red_man.h"
+#include "BitIoLdd6.h"
+#include "LED_green_man.h"
+#include "BitIoLdd7.h"
+#include "BUTTON.h"
+#include "BitIoLdd8.h"
+
+/* Inclusão de módulos compartilhados usados no projeto */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* Variáveis de controle do tempo*/
+int cont = 0;
+int cont_green = 16;
+int cont_yellow = 22;
+int cont_max = 38;
+int cont_blink = 4;
+
+/* Variáveis de controle dos periféricos */
+int button;
+int noite;
+
+int main(void){
+	/*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+	PE_low_level_init();
+	/*** End of Processor Expert internal initialization. ***/
+
+	for(;;){
+		while(noite){						// Verifica se deve operar no modo noturno
+			LED_red_car_ClrVal();			// Apaga todos os LEDs
+			LED_green_car_ClrVal();
+			LED_red_man_ClrVal();
+			LED_green_man_ClrVal();
+
+			if(!(cont%2))					// Pisca o LED amarelo
+				LED_yellow_car_ClrVal();
+			if(cont%2)
+				LED_yellow_car_SetVal();
+		}
+
+		if(cont<cont_green){				// Período do LED verde dos carros
+			LED_red_car_ClrVal();			// Somente seta o LED verde dos carros e o LED vermelho dos pedestres
+			LED_yellow_car_ClrVal();
+			LED_green_car_SetVal();
+			LED_red_man_SetVal();
+			LED_green_man_ClrVal();
+
+			if(button)
+				cont=cont_green-2;			// Aproxima o contador para o fim do período do LED verde dos carros
+		}
+		else if(cont<cont_yellow){			// Período do LED amarelo dos carros
+			LED_red_car_ClrVal();			// Somente seta o LED amarelo e o LED vermelho dos pedestres
+			LED_yellow_car_SetVal(); 	 			 
+			LED_green_car_ClrVal();
+			LED_red_man_SetVal();
+			LED_green_man_ClrVal();
+		}
+		else if(cont<cont_max-cont_blink){ 	// Período do LED vermelho dos carros
+			LED_red_car_SetVal();			// Somente seta o LED vermelho dos carros e o LED verde dos pedestres
+			LED_yellow_car_ClrVal(); 	 			 
+			LED_green_car_ClrVal();
+			LED_red_man_ClrVal();
+			LED_green_man_SetVal();
+		}
+		else{								// Últimos instantes do LED vermelho dos carros
+			LED_green_man_ClrVal();			// Somente seta o LED vermelho dos carros
+			
+			if(!(cont%2))					// Pisca o LED vermelho dos pedestres
+				LED_red_man_ClrVal();
+			if(cont%2)
+				LED_red_man_SetVal();
+		}
+	}
+
+	/*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+#ifdef PEX_RTOS_START
+	PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+#endif
+	/*** End of RTOS startup code.  ***/
+	/*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+
+} //********************************end main********************************************/
+
+/*
+ ** ###################################################################
+ **
+ **     This file was created by Processor Expert 10.3 [05.09]
+ **     for the Freescale Kinetis series of microcontrollers.
+ **
+ ** ###################################################################
+ */
